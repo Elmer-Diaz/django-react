8da13a1080d92ebc3d2dfd75d9c07a819bb40842
Elmer-Diaz <114196068+Elmer-Diaz@users.noreply.github.com>
2023-06-18 23:45:13 -0500

    termiando estilos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,24 @@
-import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { TasksPage } from "./pages/TasksPage";
 import { TaksFormPage } from "./pages/TaskFormPage";
-import { Navigation} from "./components/Navigation";
+import { Navigation } from "./components/Navigation";
+import { Toaster } from "react-hot-toast";
 
-function App(){
-  return(
+function App() {
+  return (
     <BrowserRouter>
+      <div className="container mx-auto px-4">
+        <Navigation />
 
-      <Navigation />
-
-      <Routes>
-        <Route path="/" element={<Navigate to="/tasks" />} />
-        <Route path="/tasks" element={<TasksPage />} />
-        <Route path="/tasks-create" element={<TaksFormPage />} />
-        <Route path="/tasks/:id" element={<TaksFormPage />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Navigate to="/tasks" />} />
+          <Route path="/tasks" element={<TasksPage />} />
+          <Route path="/tasks-create" element={<TaksFormPage />} />
+          <Route path="/tasks/:id" element={<TaksFormPage />} />
+        </Routes>
+      </div>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,64 +1,101 @@
-import {useEffect} from 'react'
-import {useForm} from 'react-hook-form'
-import {createTask, deleteTask, updateTask, getTask} from '../api/tasks.api'
-import {useNavigate, useParams} from 'react-router-dom'
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { createTask, deleteTask, updateTask, getTask } from "../api/tasks.api";
+import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "react-hot-toast";
 
-export function TaksFormPage(){
-
-
-  const {register, handleSubmit, formState:{errors}, setValue} = useForm()
+export function TaksFormPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm();
 
   const navigate = useNavigate();
-  const params = useParams()
+  const params = useParams();
 
-  const onsubmit = handleSubmit(async data => {
+  const onsubmit = handleSubmit(async (data) => {
     if (params.id) {
       await updateTask(params.id, data);
-    }else{
+      toast.success("Tarea Actualizada", {
+        position: "bottom-right",
+        style: {
+          color: "#fff",
+          background: "#101010",
+        },
+      });
+    } else {
       await createTask(data);
+      toast.success("Tarea Registrada", {
+        position: "bottom-right",
+        style: {
+          color: "#fff",
+          background: "#101010",
+        },
+      });
     }
     navigate("/tasks/");
-  })
+  });
 
   useEffect(() => {
     async function loadTask() {
       if (params.id) {
-        const {data} = await getTask(params.id)
-        setValue('title', data.title)
-        setValue('description', data.description)
+        const { data } = await getTask(params.id);
+        setValue("title", data.title);
+        setValue("description", data.description);
       }
     }
     loadTask();
-  }, [])
+  }, []);
 
-    return(
-      <div>
-        <form onSubmit={onsubmit}>
+  return (
+    <div className="max-w-xl mx-auto">
+      <form onSubmit={onsubmit}>
+        <input
+          type="text"
+          placeholder="Titulo"
+          {...register("title", { required: true })}
+          className="bg-zinc-700 p-3 rounded-lg block w-full mb-3"
+        />
+        {errors.title && <span> Titulo es requerido</span>}
 
-          <input type="text" placeholder="Titulo"
-          {...register("title", {required: true})} />
-          {errors.title && <span> Titulo es requerido</span>}
+        <textarea
+          rows="3"
+          placeholder="Descripción"
+          {...register("description", { required: true })}
+          className="bg-zinc-700 p-3 rounded-lg block w-full mb-3"
+        ></textarea>
+        {errors.description && <span> Descripción es requerido</span>}
 
-          <textarea  rows="3" placeholder="Descripción"
-          {...register("description", {required: true})}></textarea>
-          {errors.description && <span> Descripción es requerido</span>}
-
-          <button>Guardar</button>
-
-        </form>´
-
-      {params.id && <button onClick={async() =>{
-        const accepted = window.confirm('Estas seguro?')
-        if (accepted) {
-          await deleteTask(params.id)
-          navigate("/tasks/");
-        }
-      }}>
-        Eliminar
-      </button>}
-
-      </div>
-    )
-  }
-  
-  
+        <button className="bg-indigo-500 p-3 rounded-lg block w-full mt-3">
+          Guardar
+        </button>
+      </form>
+      ´
+      {params.id && (
+        <div className="flex justify-end">
+          <button
+            className="bg-red-500 p-3 rounded-lg w-48 mt-3"
+            onClick={async () => {
+              const accepted = window.confirm("Estas seguro?");
+              if (accepted) {
+                await deleteTask(params.id);
+                toast.success("Tarea Eliminada", {
+                  position: "bottom-right",
+                  style: {
+                    color: "#fff",
+                    background: "#101010",
+                  },
+                });
+                navigate("/tasks/");
+              }
+            }}
+          >
+            Eliminar
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
